f2161a946ab248ce5e07df0b7c0142bcbe0bcd6f
Bartosz Lipiński <bartosz.s.lipinski@gmail.com>
2023-12-10 13:22:08 +0100

    feat: declare routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
-import { Routes } from '@angular/router';
+import { Routes as TRoutes } from '@angular/router';
+import { Routes } from './routes';
 
-export const routes: Routes = [];
+export const routes: TRoutes = [
+  {
+    path: Routes.HOME,
+    redirectTo: Routes.DASHBOARD,
+    pathMatch: 'full',
+  },
+  {
+    path: Routes.DASHBOARD,
+    loadComponent: () =>
+      import('./dashboard/dashboard.component').then(
+        (m) => m.DashboardComponent,
+      ),
+    title: 'Dashboard | HR Management',
+  },
+  {
+    path: Routes.SIGNIN,
+    loadComponent: () =>
+      import('./sign-in/sign-in.component').then((m) => m.SignInComponent),
+    title: 'Sign In | HR Management',
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./not-found/not-found.component').then(
+        (m) => m.NotFoundComponent,
+      ),
+    title: 'Not Found | HR Management',
+  },
+];
---
+++ src/app/routes.ts
@@ -0,0 +1,5 @@
+export enum Routes {
+  HOME = '',
+  DASHBOARD = 'dashboard',
+  SIGNIN = 'sign-in',
+}
