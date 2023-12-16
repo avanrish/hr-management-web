43788166dfac49a38fc1631b946bf49bc6a3eaa6
Bartosz Lipiński <bartosz.s.lipinski@gmail.com>
2023-12-16 18:27:34 +0100

    feat: 404 page

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Routes } from '../routes';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './not-found.component.html',
 })
-export class NotFoundComponent {}
+export class NotFoundComponent {
+  protected readonly Routes = Routes;
+}
